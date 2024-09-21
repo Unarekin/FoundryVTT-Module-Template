07e010372bb1428ee35f085cdcec844b325c6d58
Unarekin <erica@blackspork.com>
2024-09-21 03:28:19 -0500

    Updating build

--- build.mjs
+++ build.mjs
@@ -5,89 +5,123 @@ import { clean as cleanPlugin } from "esbuild-plugin-clean";
 import copyPlugin from "esbuild-copy-static-files";
 import { sassPlugin } from "esbuild-sass-plugin";
 import { nodeExternalsPlugin } from "esbuild-node-externals";
-import { glob } from "glob";
-import path from "path";
 import jsonMerge from "esbuild-plugin-json-merge";
+import path from "path";
+import { promises as fs } from "fs";
+import { deepmerge } from "deepmerge-ts";
+import yoctoSpinner from "yocto-spinner";
+import { ESLint } from "eslint";
 
-import moduleSpec from "./module.json" with { "type": "json" };
+/** Paths */
+const SRC_PATH = "./src";
+const LANG_PATH = path.join(SRC_PATH, "languages");
+const OUT_PATH = "./dist";
+const STYLE_PATH = path.join(SRC_PATH, "styles");
+const TEMPLATE_PATH = path.join(SRC_PATH, "templates");
 
-/** Convenience variable to determine if this is a development or prodution buld. */
-const __DEV__ = process.env.NODE_ENV !== "production" ? true : false;
+/** Build a simple version of the copy plugin with default settings */
+function simpleCopy(src, dest) {
+  return copyPlugin({
+    src,
+    dest,
+    dereference: true,
+    errorOnExist: false,
+    preserveTimestamps: true,
+  });
+}
 
-/**
- * Builds a list of JSON language sources, to be merged during build step.
- */
-const languageDefs = await (async () => {
-  const templates = (await glob("./src/languages/**/*", { nodir: true })).map(
-    (dir) => path.relative("./src/languages", dir)
-  );
-  const languages = templates
-    .map((template) => template.split(path.sep)[0])
-    .filter((dir, index, arr) => arr.indexOf(dir) === index);
+// Import module.json for some config options
+import moduleConfig from "./module.json" assert { type: "json" };
 
-  return Object.fromEntries(
-    languages.map((lang) => [
-      lang,
-      templates
-        .filter((template) => template.split(path.sep)[0] === lang)
-        .map((file) => path.join("./src/languages/", file)),
-    ])
-  );
-})();
+// Constants to be inserted into process.env during build
+const __DEV__ = process.env.NODE_ENV !== "production";
+const __MODULE_TITLE__ = moduleConfig.title;
+const __MODULE_ID__ = moduleConfig.id;
+const __MODULE_VERSION__ = moduleConfig.version;
 
-const results = await build({
-  entryPoints: ["./src/module.ts", "./src/styles/module.scss"],
-  outdir: "./dist",
+const start = Date.now();
+let spinner = null;
+
+if (!process.argv.slice(2).includes("--no-lint")) {
+  const lintStart = Date.now();
+  spinner = yoctoSpinner({ text: "Linting..." }).start();
+
+  const linter = new ESLint({
+    cache: true,
+    errorOnUnmatchedPattern: false,
+  });
+  const lintResults = await linter.lintFiles(["src/**.ts", "src/*/**.ts"]);
+  await ESLint.outputFixes(lintResults);
+
+  let formatter = await linter.loadFormatter("html");
+  await fs.writeFile("./lint-report.html", formatter.format(lintResults));
+
+  const hasErrors = lintResults.findIndex((result) => result.errorCount) !== -1;
+  if (hasErrors) {
+    spinner.error("Linting errors found!");
+    formatter = await linter.loadFormatter("stylish");
+    console.log(formatter.format(lintResults));
+    process.exit();
+  } else {
+    spinner.success(
+      `Linting passed in ${((Date.now() - start) / 1000).toFixed(2)}s`
+    );
+  }
+}
+
+const buildStart = Date.now();
+spinner = yoctoSpinner({ text: "Building..." }).start();
+const jsonMergers = (
+  await fs.readdir(LANG_PATH, { withFileTypes: true })
+).reduce((prev, curr) => {
+  if (curr.isDirectory())
+    return [
+      ...prev,
+      jsonMerge({
+        entryPoints: [path.join(LANG_PATH, curr.name, "*.json")],
+        outfile: path.join("languages", `${curr.name}.json`),
+        merge: (items) => deepmerge(...items),
+      }),
+    ];
+  else return prev;
+}, []);
+
+const buildResults = await build({
+  entryPoints: [
+    path.join(SRC_PATH, "module.ts"),
+    path.join(STYLE_PATH, "module.scss"),
+  ],
+  outdir: OUT_PATH,
+  sourcemap: __DEV__,
   bundle: true,
   platform: "browser",
+  minify: !__DEV__,
   define: {
-    __DEV__: process.env.NODE_ENV === "production" ? "true" : "false",
-    __MODULE_TITLE__: `"${moduleSpec.title}"`,
-    __MODULE_ID__: `"${moduleSpec.id}"`,
-    __MODULE_VERSION__: `"${moduleSpec.version}"`
+    __DEV__: __DEV__ === true ? "true" : "false",
+    __MODULE_TITLE__: `"${__MODULE_TITLE__}"`,
+    __MODULE_ID__: `"${__MODULE_ID__}"`,
+    __MODULE_VERSION__: `"${__MODULE_VERSION__}"`,
   },
+  external: ["*.woff", "*.woff2", "*.otf", "*.ttf", "*.webp"],
   plugins: [
     nodeExternalsPlugin(),
-    cleanPlugin({
-      patterns: ["./dist/**/*"],
-    }),
+    cleanPlugin({ patterns: "./dist/**" }),
     sassPlugin(),
-    copyPlugin({
-      src: "./module.json",
-      dest: "./dist/module.json",
-      dereference: true,
-      errorOnExist: false,
-      preserveTimestamps: true,
-    }),
-    copyPlugin({
-      src: "./src/templates",
-      dest: "./dist/templates",
-      dereference: true,
-      errorOnExist: false,
-      preserveTimestamps: true,
-    }),
-    copyPlugin({
-      src: "./LICENSE",
-      dest: "./dist/LICENSE",
-      dereference: true,
-      errorOnExist: false,
-      preserveTimestamps: true,
-    }),
-        copyPlugin({
-      src: "./README.md",
-      dest: "./dist/README.md",
-      dereference: true,
-      errorOnExist: false,
-      preserveTimestamps: true,
-    }),
-    ...Object.entries(languageDefs).map(([lang, entryPoints]) =>
-      jsonMerge({
-        entryPoints,
-        outfile: path.join("./languages", `${lang}.json`),
-      })
-    ),
+    simpleCopy("./module.json", path.join(OUT_PATH, "module.json")),
+    simpleCopy(TEMPLATE_PATH, path.join(OUT_PATH, "templates")),
+    simpleCopy(path.join(SRC_PATH, "fonts"), path.join(OUT_PATH, "fonts")),
+    simpleCopy("./LICENSE", path.join(OUT_PATH, "LICENSE")),
+    simpleCopy("./README.md", path.join(OUT_PATH, "README.md")),
+    ...jsonMergers,
   ],
 });
 
-if (results.warnings.length) console.warn(results.warnings);
-if (results.errors.length) console.error(result.errors);
+if (buildResults.errors.length) {
+  spinner.error("Build failed!");
+  console.error(buildResults.errors);
+} else {
+  spinner.success(
+    `Build completed in ${((Date.now() - buildStart) / 1000).toFixed(2)}s`
+  );
+  if (buildResults.warnings.length) console.warn(buildResults.warnings);
+}
